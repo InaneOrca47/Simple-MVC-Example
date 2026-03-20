c9bb6877f0d0420cb52f73f0c9f835cf15abb90d
Benjamin Kingsley (RIT Student) <bdk8938@rit.edu>
2026-03-20 12:49:11 -0400

    Starting mongodb

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
 // pull in our models. This will automatically load the index.js from that folder
-const models = require('../models');
+const { Cat } = require('../models');
 
 const hostIndex = (req, res) => {
   let name = 'unknown';
@@ -27,11 +27,29 @@ const getName = (req, res) => {
 
 };
 
-const setName = (req, res) => {
+const setName = async (req, res) => {
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
     return res.status(400).json({ error: 'firstname, lastname and beds are all required' });
   }
-  
+
+  const catData = {
+    name: `${req.body.firstname} ${req.body.lastname}`,
+    bedsOwned: req.body.beds,
+  }
+
+  const newCat = new Cat(catData);
+
+  try {
+    await newCat.save();
+    return res.status(201).json({
+      name: newCat.name,
+      beds: newCat.bedsOwned,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Failed to create cat.' });
+  }
+
 };
 
 const searchName = (req, res) => {
@@ -41,7 +59,7 @@ const searchName = (req, res) => {
 };
 
 const updateLast = (req, res) => {
-	
+
 };
 
 const notFound = (req, res) => {
