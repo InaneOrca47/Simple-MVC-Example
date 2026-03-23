5a687a3d81e43c92412177ab8394a941dd14723e
Benjamin Kingsley (RIT Student) <bdk8938@rit.edu>
2026-03-23 12:50:40 -0400

    finished cats Model demo

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,20 @@
 // pull in our models. This will automatically load the index.js from that folder
 const { Cat } = require('../models');
 
-const hostIndex = (req, res) => {
+const hostIndex = async (req, res) => {
   let name = 'unknown';
+
+  try {
+    const doc = await Cat.findOne({}, {}, {
+      sort: { 'createdDate': 'descending' }
+    }).lean().exec();
+
+    if (doc) {
+      name = doc.name
+    }
+  } catch (err) {
+    console.log(err);
+  }
 
   res.render('index', {
     currentName: name,
@@ -11,8 +23,16 @@ const hostIndex = (req, res) => {
   });
 };
 
-const hostPage1 = (req, res) => {
-
+const hostPage1 = async (req, res) => {
+  try {
+    const docs = await Cat.find({}).lean().exec();
+    return res.render('page1', {
+      cats: docs,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'failed to find cats.' });
+  }
 };
 
 const hostPage2 = (req, res) => {
@@ -29,7 +49,7 @@ const getName = (req, res) => {
 
 const setName = async (req, res) => {
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
-    return res.status(400).json({ error: 'firstname, lastname and beds are all required' });
+    return res.status(400).json({ error: 'firstname, lastname and beds are all required.' });
   }
 
   const catData = {
@@ -47,19 +67,47 @@ const setName = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: 'Failed to create cat.' });
+    return res.status(500).json({ error: 'failed to create cat.' });
   }
-
 };
 
-const searchName = (req, res) => {
+const searchName = async (req, res) => {
   if (!req.query.name) {
-    return res.status(400).json({ error: 'Name is required to perform a search' });
+    return res.status(400).json({ error: 'name is required to perform a search' });
+  }
+
+  try {
+    const doc = await Cat.findOne({ name: req.query.name }).select('name bedsOwned').exec();
+
+    if (!doc) {
+      return res.status(404).json({ error: 'no cat found' });
+    }
+
+    return res.status(200).json({ name: doc.name, beds: doc.bedsOwned });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'something went wrong' });
   }
 };
 
 const updateLast = (req, res) => {
+  // no await async
+  const updatePromise = Cat.findOneAndUpdate({}, { $inc: { bedsOwned: 1 } }, {
+    returnDocument: 'after',
+    sort: { 'createdDate': 'descending' }
+  }).lean().exec();
 
+  updatePromise.then(doc => {
+    return res.json({
+      name: doc.name,
+      beds: doc.bedsOwned
+    });
+  });
+
+  updatePromise.catch(err => {
+    console.log(err);
+    return res.status(500).json({ error: 'update failed' });
+  })
 };
 
 const notFound = (req, res) => {
